81af8a4d95b8e54241a07150cc6e6eb98adbc212
Niklas Voss <niklas.voss@gmail.com>
2025-02-01 13:44:32 +0100

    Use nftables instead, add ntp, dns and dhcpv6 to adv ntp

--- modules/router.nix
+++ modules/router.nix
@@ -64,29 +64,13 @@ in
 
     boot.kernel.sysctl = {
       "net.ipv4.ip_nonlocal_bind" = true;
+      "net.ipv4.conf.all.forwarding" = true;
+      "net.ipv6.conf.all.forwarding" = true;
+      "net.ipv6.conf.${cfg.lanIf}.accept_ra" = false;
     };
 
     networking = {
-      firewall = {
-        enable = true;
-        trustedInterfaces = [
-          "tailscale0"
-        ];
-        pingLimit = "--limit 1/minute --limit-burst 5";
-        allowedUDPPorts = [ 51820 ]; # wg
-        allowedTCPPorts = [ 22 ];
-
-        extraCommands = ''
-          # Allow VRRP and AH packets
-          ip6tables -A nixos-fw -i ${cfg.lanIf} -p vrrp -j ACCEPT
-          ip6tables -A nixos-fw -i ${cfg.lanIf} -p ah -j ACCEPT
-        '';
-
-        extraStopCommands = ''
-          ip6tables -D nixos-fw -i ${cfg.lanIf} -p vrrp -j ACCEPT || true
-          ip6tables -D nixos-fw -i ${cfg.lanIf} -p ah -j ACCEPT || true
-        '';
-      };
+      firewall.enable = false; # we are using nftables
       useDHCP = false; # we are using networkd
     };
 
@@ -109,6 +93,46 @@ in
         };
       };
     };
+
+    nftables = {
+      enable = true;
+      ruleset = ''
+        table ip filter {
+          chain input {
+            type filter hook input priority 0; policy drop;
+
+            iifname lo accept comment "Allow connections from loopback"
+            iifname { "${cfg.lanIf}" } accept comment "Allow local network to access the router"
+            iifname "${cfg.wanIf}" ct state { established, related } accept comment "Allow established traffic"
+            iifname "${cfg.wanIf}" icmp type { echo-request, destination-unreachable, time-exceeded } counter accept comment "Allow select ICMP"
+            tcp dport {ssh} accept comment "Allow SSH connections to the router"
+            iifname "${cfg.wanIf}" counter drop comment "Drop all other unsolicited traffic from wan"
+          }
+          chain forward {
+            type filter hook forward priority 0; policy drop;
+            iifname { "${cfg.lanIf}" } oifname { "${cfg.wanIf}" } accept comment "Allow trusted LAN to WAN"
+            iifname { "${cfg.wanIf}" } oifname { "${cfg.lanIf}" } ct state established, related accept comment "Allow established back to LANs"
+          }
+        }
+
+        table ip nat {
+          chain postrouting {
+            type nat hook postrouting priority 100; policy accept;
+            oifname "${cfg.wanIf}" masquerade
+          }
+        }
+
+        table ip6 filter {
+	        chain input {
+            type filter hook input priority 0; policy drop;
+          }
+          chain forward {
+            type filter hook forward priority 0; policy drop;
+          }
+        }
+      '';
+    };
+
 
     # https://fy.blackhats.net.au/blog/2018-11-01-high-available-radvd-on-linux/
     services.keepalived = {
@@ -140,6 +164,7 @@ in
       '';
     };
 
+    # IPv6 RA
     services.radvd = {
       enable = true;
       config = ''
@@ -151,6 +176,9 @@ in
             AdvRASrcAddress {
                 fe80::1:1;
             };
+            RDNSS ${builtins.elemAt (lib.splitString cfg.vipAddress) 0}{
+                AdvRDNSSLifetime 3600;
+            };
             prefix ${cfg.raPrefix}
             {
                 AdvOnLink on;
@@ -158,6 +186,54 @@ in
                 AdvRouterAddr off;
             };
         };
+      '';
+    };
+
+    # DHCPv6
+    services.kea.ctrl-agent.enable = false;
+    services.kea.dhcp6 = {
+      enable = true;
+      settings = {
+        interface-config = {
+          interfaces = [ cfg.lanIf ];
+        };
+        option-data = [
+          {
+            name = "ntp-servers";
+            data = "${builtins.elemAt (lib.splitString cfg.vipAddress) 0}";
+          }
+          {
+            name = "dns-servers";
+            data = "${builtins.elemAt (lib.splitString cfg.vipAddress) 0}";
+          }
+        ];
+        lease-database = {
+          type = "memfile";
+        };
+      };
+    };
+
+    # NTP
+    services.chrony = {
+      enable = true;
+      extraConfig = ''
+        allow ${cfg.raPrefix}
+      '';
+    };
+
+    # DNS
+    services.coredns = {
+      enable = true;
+      config = ''
+        .:53 {
+          forward . 8.8.8.8
+          log
+          errors
+          cache
+          dns64 {
+            allow_ipv4
+          }
+        }
       '';
     };
 
